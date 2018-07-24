b0851601b3568e5f081f170ed308817d8a8f30a9
Tommie Behrenbeck <behrenbt@gmail.com>
2018-07-24 18:08:27 -0500

    displayOnScreen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,23 +2,24 @@ var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var wins = 0;
 var losses = 0;
 
-document.onkeyup = function() {
+document.onkeyup = function () {
     var userguess = String.fromCharCode(event.keyCode).toLowerCase();
 
     console.log(userguess);
 
 
-var computerGuess = options[Math.floor(Math.random()* options.length)];
+    var computerGuess = options[Math.floor(Math.random() * options.length)];
 
     console.log(computerGuess);
 
     if (userguess === computerGuess) {
         wins++
-        alert("Wins: " + wins)
     } else {
         losses++
-        alert("Losses: " + losses);
     }
 
+    var html = "<h1>Psychic Game</h1>" + "<p>Guess what letter I'm thinking of</p>" + "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>";
+
+    document.querySelector('#game').innerHTML = html;
 
 }
