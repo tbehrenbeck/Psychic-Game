9a93e884f7d68e2dcbeee028dfd98872623a327a
Tommie Behrenbeck <behrenbt@gmail.com>
2018-07-24 19:59:32 -0500

    organizeCode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,38 @@
+
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
+var guessesLeft = 10;
+var guessesSoFar = [];
+var computersLetter = options[Math.floor(Math.random() * options.length)];
 
-document.onkeyup = function () {
-    var userguess = String.fromCharCode(event.keyCode).toLowerCase();
+document.onkeyup = function(event) {
 
-    console.log(userguess);
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
+	
+	if (guessesSoFar.indexOf(userGuess) < 0 && options.indexOf(userGuess) >= 0) {
+		guessesSoFar.push(userGuess);
+		guessesLeft--;
+	}
 
-    var computerGuess = options[Math.floor(Math.random() * options.length)];
+	if (computersLetter == userGuess) {
+		wins++;
+		console.log("You won!");
+		guessesLeft = 9;
+		guessesSoFar = [];
+		computersLetter = options[Math.floor(Math.random() * options.length)];
+	}
 
-    console.log(computerGuess);
+	if (guessesLeft == 0) {
+		losses++;
+		console.log("You lost!");
+		guessesLeft = 9;
+		guessesSoFar = [];
+		computersLetter = options[Math.floor(Math.random() * options.length)];
+	}
 
-    if (userguess === computerGuess) {
-        wins++
-    } else {
-        losses++
-    }
-
-    var html = "<h1>Psychic Game</h1>" + "<p>Guess what letter I'm thinking of</p>" + "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>";
-
-    document.querySelector('#game').innerHTML = html;
+	var html = "<p><h1>Guessing Game</h1></p>" + "<p><h4>Guess what letter am I thinking of...</h4></p>" + "<p><h4>Wins: " + wins + "</h4></p>" + "<p><h4>Losses: " + losses + "</h4></p>" + "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + "<p><h4>Your guesses so far: " + guessesSoFar + "</h4></p>";
+	document.querySelector("#game").innerHTML = html;
 
 }
