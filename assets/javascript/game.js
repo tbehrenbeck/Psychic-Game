40de859e5a570e772cc9894103490ab39798ea56
Tommie Behrenbeck <behrenbt@gmail.com>
2018-07-25 19:51:55 -0500

    formatting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,34 +5,37 @@ var losses = 0;
 var guessesLeft = 10;
 var guessesSoFar = [];
 var computersLetter = options[Math.floor(Math.random() * options.length)];
+console.log(computersLetter);
+
 
 document.onkeyup = function(event) {
 
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	
 
 	
 	if (guessesSoFar.indexOf(userGuess) < 0 && options.indexOf(userGuess) >= 0) {
 		guessesSoFar.push(userGuess);
 		guessesLeft--;
-	}
-
+	} 
+	
 	if (computersLetter == userGuess) {
 		wins++;
 		console.log("You won!");
-		guessesLeft = 9;
+		guessesLeft = 10;
 		guessesSoFar = [];
 		computersLetter = options[Math.floor(Math.random() * options.length)];
-	}
-
-	if (guessesLeft == 0) {
+		console.log(computersLetter);
+	} else if (guessesLeft == 0) {
 		losses++;
 		console.log("You lost!");
-		guessesLeft = 9;
+		guessesLeft = 10;
 		guessesSoFar = [];
 		computersLetter = options[Math.floor(Math.random() * options.length)];
+		console.log(computersLetter);		
 	}
 
-	var html = "<p><h1>Guessing Game</h1></p>" + "<p><h4>Guess what letter am I thinking of...</h4></p>" + "<p><h4>Wins: " + wins + "</h4></p>" + "<p><h4>Losses: " + losses + "</h4></p>" + "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + "<p><h4>Your guesses so far: " + guessesSoFar + "</h4></p>";
+	var html = "<p><h1>Guessing Game</h1></p>" + "<p><h4>Guess what letter I am thinking of...</h4></p>" + "<p><h4>Wins: " + wins + "</h4></p>" + "<p><h4>Losses: " + losses + "</h4></p>" + "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + "<p><h4>Your guesses so far: " + guessesSoFar + "</h4></p>";
 	document.querySelector("#game").innerHTML = html;
 
 }
